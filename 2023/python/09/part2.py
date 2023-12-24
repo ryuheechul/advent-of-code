f5daf17de136113b8914d159f748a352de96d03e
Heechul Ryu <ryuhcii@gmail.com>
2023-12-24 13:53:08 -0800

    add day 9

---
+++ 2023/python/09/part2.py
@@ -0,0 +1,35 @@
+# https://adventofcode.com/2023/day/9
+
+
+def parse():
+    with open("input.txt", "r") as reader:
+        sequences = [list(map(int, line.split())) for line in reader.readlines()]
+
+        return sequences
+
+
+def calc_diffs(seq: list[int]):
+    lefty = seq[0:-1]
+    righty = seq[1:]
+
+    return [r - l for l, r in zip(lefty, righty)]
+
+
+def predict(seq: list[int]):
+    first = seq[0]
+    diffs = calc_diffs(seq)
+
+    is_all_0 = all(diff == 0 for diff in diffs)
+
+    if is_all_0:
+        return first
+
+    return first - predict(diffs)
+
+
+if __name__ == "__main__":
+    seqs = parse()
+
+    answer = sum(predict(s) for s in seqs)
+
+    print(answer)

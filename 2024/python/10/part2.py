d3b31a3c2dd277684a15fdddc633fa61ee923049
Heechul Ryu <ryuhcii@gmail.com>
2024-12-09 23:48:34 -0800

    add day 10

---
+++ 2024/python/10/part2.py
@@ -0,0 +1,50 @@
+# https://adventofcode.com/2024/day/10
+
+Grid = list[list[int]]
+Coords = tuple[int, int]
+
+
+def find_trail_heads(grid: Grid):
+    for y, line in enumerate(grid):
+        for x, height in enumerate(line):
+            if height == 0:
+                yield (x, y)
+
+
+def traverse(height: int, current: Coords, grid: Grid):
+    x, y = current
+    ly = len(grid)
+    lx = len(grid[0])
+
+    if x < 0 or y < 0 or x > lx - 1 or y > ly - 1:
+        return 0
+
+    if grid[y][x] != height:
+        return 0
+
+    if height == 9:
+        return 1
+
+    north = x, y - 1
+    east = x + 1, y
+    south = x, y + 1
+    west = x - 1, y
+
+    return sum(
+        traverse(height + 1, next_spot, grid)
+        for next_spot in [north, east, south, west]
+    )
+
+
+def score(head: Coords, grid: Grid):
+    return traverse(0, head, grid)
+
+
+filename = ["sample1.txt", "input.txt"][1]
+with open(filename, "r") as reader:
+    grid = [[int(h) for h in line.strip()] for line in reader.readlines()]
+
+    heads = list(find_trail_heads(grid))
+
+    s = sum(score(head, grid) for head in heads)
+    print(s)
